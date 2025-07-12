ba45fbae58ef6e9193a1038679d2b2ff6c1e0d2d
shiva <shiva@shivani>
2025-07-12 12:36:35 +0530

    Constructor final commit

--- src/eleven/April/DemoS.java
+++ src/eleven/April/DemoS.java
@@ -3,7 +3,7 @@ package eleven.April;
 public class DemoS{
 public static void main(String[] args){
 
-State s = new State("Maharashtra", 444806);
+State s = new State("MaharashtraMH", 444806);
 s.printDetails();
 
 }
